b12e777c4a0e8149789e6f387215cabe43c1c4ca
dlau <danielau88@gmail.com>
2023-04-12 22:24:11 +0800

    Change name to c academy

--- src/commons/application/ApplicationTypes.ts
+++ src/commons/application/ApplicationTypes.ts
@@ -179,7 +179,11 @@ export const defaultPlayground: PlaygroundState = {
   githubSaveInfo: { repoName: '', filePath: '' }
 };
 
-export const defaultEditorValue = '1+1';
+export const defaultEditorValue = `
+  int main() {
+    return 0;
+  }
+`;
 
 /**
  * Create a default IWorkspaceState for 'resetting' a workspace.
